2ba6f124b2f6f8dab285a3d9fdf9446c9c64920e
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-23 10:08:17 +0545

    check if digit

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ import sys
 
 def match_pattern(input_line: str, pattern: str):
     if pattern == "\\d":
-        return any(char.isdigit for char in input_line)
+        return any(char.isdigit() for char in input_line)
     if len(pattern) == 1:
         return pattern in input_line
     else:
