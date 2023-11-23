7663cf61786c620494dd0cb13bdd5f20e0734436
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-23 10:34:48 +0545

    match word character

--- app/main.py
+++ app/main.py
@@ -4,11 +4,30 @@ import sys
 # import lark - available if you need it!
 
 
-def match_pattern(input_line: str, pattern: str):
+def match_digit(line):
+    return any(char.isdigit() for char in line)
+
+
+def match_words(line):
+    for char in line:
+        if ord(char) >= ord("0") and ord(char) <= ord("9"):
+            return True
+        if ord(char) >= ord("A") and ord(char) <= ord("Z"):
+            return True
+        if ord(char) >= ord("a") and ord(char) <= ord("z"):
+            return True
+        if char == "_":
+            return True
+    return False
+
+
+def match_pattern(line: str, pattern: str):
+    if pattern == "\\w":
+        return match_words(line)
     if pattern == "\\d":
-        return any(char.isdigit() for char in input_line)
+        return match_digit(line)
     if len(pattern) == 1:
-        return pattern in input_line
+        return pattern in line
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
